54ca04f59164d2a82a72298e1177e17109581be4
SpaniardRos <referatguru@yandex.ru>
2022-08-02 10:18:08 +0300

    correct @Transactional

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -11,7 +11,6 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-//@Transactional
 public class UserDaoImpl implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
